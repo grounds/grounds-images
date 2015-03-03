35b50522a648e5548f460c6ba85c22eeca4306d1
Adrien Folie <folie.adrien@gmail.com>
2015-03-03 01:07:15 +0100

    add travis build config

--- spec/features/run_examples_spec.rb
+++ spec/features/run_examples_spec.rb
@@ -3,13 +3,16 @@ require 'container'
 require 'example'
 
 describe 'Run code examples' do
+  after(:each) do
+    @container.remove
+  end
+
   Example.all.each do |example|
     it "#{example.language} #{example.title} has proper output" do
-      container = Container.new(example)
-      container.run
-      container.remove
+      @container = Container.new(example)
+      @container.run
 
-      expect(container.output).to eq(example.output)
+      expect(@container.output).to eq(example.output)
     end
   end
 end
