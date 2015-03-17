55636f760eb429ed0315493b2b9e5aa28feb0cb7
Gaëtan Gueraud <ggueraud@unowhy.com>
2015-03-17 17:37:12 +0100

    only_changed

--- lib/image.rb
+++ lib/image.rb
@@ -43,12 +43,14 @@ class Image
   def self.only_changed
     languages = []
     diff = `git diff --name-only`
+          puts "DEBUG " + diff
     diff.split(/\r?\n/).each {|line| 
       if line.start_with?('example', DIR)
         languages.push line.gsub("([^\/:]+)\/[^\/]+$", "") + ".#{EXTENSION}"
       elsif line.start_with?(DIR)
         languages.push line.gsub("\/(.[[:alpha:]]+)", "") + ".#{EXTENSION}"
       end}
+      puts "DEBUG " + languages
     return find 'ruby'  if languages.empty?
     languages.map(&method(:new))
   end
